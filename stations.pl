e7542d1c407b55f9a37f22b76056981f6c4f81eb
Rogelio Torres <87195114+rogergelio@users.noreply.github.com>
2022-06-12 21:52:57 -0500

    Add files via upload

---
+++ stations.pl
@@ -0,0 +1,132 @@
+%yellow line south-north
+
+:- discontigious station(metro, huntington, 38.7967704,-77.0809362,1, 1).
+:- discontigious station(metro, eisenhower_avenue, 38.8003582,-77.0733477,1, 2).
+:- discontigious station(metro, king_st, 38.8071786,-77.0622822,1, 3). %cruza con azul
+:- discontigious station(metro, braddock_rd, 38.8140243,-77.0559649,1,4).%cruza con azul
+:- discontigious station(metro, national_airport, 38.8533867,-77.0461755,1,5).%cruza con azul
+:- discontigious station(metro, crystal_city, 38.8549359,-77.0538342,1,6). %cruza con azul
+:- discontigious station(metro, pentagon_city, 38.8627106,-77.0620722,1, 7). %cruza con azul
+:- discontigious station(metro, pentagon, 38.8686532,-77.0565887,1, 8).%cruza con azul
+:- discontigious station(metro, lenfant_plaza, 38.886256,-77.0237502, 1, 9).%cruza con todas
+:- discontigious station(metro, archives, 38.893904,-77.0244128,1, 10). %cruza con verde
+:- discontigious station(metro, gallery_chinatown, 38.8981511,-77.0258766,1,11).%cruza con verde
+:- discontigious station(metro, mt_vernon_sq, 38.8992832,-77.0325695,1,12).%cruza con verde
+:- discontigious station(metro, shaw_howard, 38.8992832,-77.0325695,1,13).%cruza con verde
+:- discontigious station(metro, u_street, 38.9168309,-77.0312974, 1, 14).%cruza con verde
+:- discontigious station(metro, columbia_street, 38.9289062,-77.0346292,1, 15).%cruza con verde
+:- discontigious station(metro, georgia_petworth, 38.9369458,-77.0263745,1, 16).%cruza con verde
+:- discontigious station(metro, fort_totten, 38.9522993,-77.003775,1,17).%cruza con verde y rojo
+
+%2 line: south-east
+
+:- discontigious station(metro, franconia_springfield, 38.7665102,-77.1701807,2, 1).
+:- discontigious station(metro, van_dorn, 38.7992958,-77.1314206, 2, 2).
+:- discontigious station(metro, king_st, 38.8071786,-77.0622822,2, 3). %cruza con amarillo
+:- discontigious station(metro, braddock_rd, 38.8140243,-77.0559649,2,4).%cruza con amarillo
+:- discontigious station(metro, national_airport, 38.8533867,-77.0461755,2,5).%cruza con amarillo
+:- discontigious station(metro, crystal_city, 38.8549359,-77.0538342,2,6).%cruza con amarillo
+:- discontigious station(metro, pentagon_city, 38.8627106,-77.0620722,2, 7). %cruza con amarillo
+:- discontigious station(metro, pentagon, 38.8686532,-77.0565887,2, 8).%cruza con amarillo
+:- discontigious station(metro, arlington_cementery, 38.884591,-77.0658303,2,9).
+:- discontigious station(metro, rosslyn, 38.8958074,-77.0740979,2,10).%cruza con naranja
+:- discontigious station(metro, foggy_bottom, 38.9008738,-77.0526492,2, 11). %cruza con naranja
+:- discontigious station(metro, farragut_west, 38.9014818,-77.0442156,2,b12).%cruza con naranja
+:- discontigious station(metro, mcpherson_sq, 38.9011445,-77.0344226,2, b13).%cruza con naranja
+:- discontigious station(metro, metro_center, 38.8985241,-77.0300918,2,14).%cruza con naranja y rojo
+:- discontigious station(metro, federal_triangle, 38.8937777,-77.0305082,2, 15).%cruza con naranja
+:- discontigious station(metro, smithsonian, 38.8890827,-77.030559,2, 16).%cruza con naranja
+:- discontigious station(metro, lenfant_plaza, 38.886256,-77.0237502, 2, 17).%cruza con todas
+:- discontigious station(metro, federal_center_sw, 38.8848747,-77.0177512,2,18).%cruza con naranja
+:- discontigious station(metro, capitol_south, 38.8856475,-77.0082409,2, 19).% cruza con naranja
+:- discontigious station(metro, eastern_market, 38.8842742,-76.9980307,2, 20).%cruza con naranja
+:- discontigious station(metro, potomac_avenue, 38.8808519,-76.9873879,2, 21). %cruza con naranja
+:- discontigious station(metro, stadium_armory, 38.8883062,-76.9792615,2, 22).%cruza con naranja
+:- discontigious station(metro, benning, 38.8903937,-76.939636, 2, 23).
+:- discontigious station(metro, capitol_heights, 38.8892561,-76.9154002,2, 24).
+:- discontigious station(metro, addison_rd, 38.8867256,-76.8977313,2, 25).
+:- discontigious station(metro, morgan, 38.8936965,-76.8706695, 2, 26).
+:- discontigious station(metro, largo_town, 38.9006019,-76.846751,2, 27).
+
+%orange: west-east
+:- discontigious station(metro, vienna_fairfax, 38.8775832,-77.2746497, 3, 1).
+:- discontigious station(metro, dunn_loring, 38.8823412,-77.2315867, 3, 2).
+:- discontigious station(metro, west_falls_church, 38.900786,-77.1912609, 3, 3).
+:- discontigious station(metro, east_falls_church, 38.885555,-77.1581505, 3, 4).
+:- discontigious station(metro, ballston, 38.8819174,-77.1123916, 3, 5).
+:- discontigious station(metro, virgina_sq, 38.8828717,-77.1055041, 3, 6).
+:- discontigious station(metro, clarendon, 38.8871076,-77.0974038, 3, 7).
+:- discontigious station(metro, courthouse, 38.8911704,-77.0872124, 3, 8).
+:- discontigious station(metro, rosslyn, 38.8958074,-77.0740979,3,9).%cruza con azul
+:- discontigious station(metro, foggy_bottom, 38.9008738,-77.0526492,3,10). %cruza con azul
+:- discontigious station(metro, farragut_west, 38.9014818,-77.0442156,3, 11).%cruza con azul
+:- discontigious station(metro, mcpherson_sq, 38.9011445,-77.0344226,3, 12).%cruza con azul
+:- discontigious station(metro, metro_center, 38.8985241,-77.0300918,313).%cruza con azul y rojo
+:- discontigious station(metro, federal_triangle, 38.8937777,-77.0305082,3, o14).%cruza con azul
+:- discontigious station(metro, smithsonian, 38.8890827,-77.030559,3, 15).%cruza con azul
+:- discontigious station(metro, lenfant_plaza, 38.886256,-77.0237502, 3, 16).%cruza con todas
+:- discontigious station(metro, federal_center_sw, 38.8848747,-77.0177512,3, 17).%cruza con azul
+:- discontigious station(metro, capitol_south, 38.8856475,-77.0082409,3, 18).% cruza con azul
+:- discontigious station(metro, eastern_market, 38.8842742,-76.9980307,3, 19).%cruza con azul
+:- discontigious station(metro, potomac_avenue, 38.8808519,-76.9873879,3, 20). %cruza con azul
+:- discontigious station(metro, stadium_armory, 38.8883062,-76.9792615,3, 21).%cruza con azul
+:- discontigious station(metro, minnesota_avenue, 38.8991338,-76.9489688, 3, 22).
+:- discontigious station(metro, deanwood, 38.9081925,-76.9373711, 3, 23).
+:- discontigious station(metro, cheverly, 38.9161491,-76.9184529, 3, 24).
+:- discontigious station(metro, landlover, 38.9335082,-76.8934481, 3, 25).
+:- discontigious station(metro, new_carrollton, 38.9474648,-76.874289, 3, 26).
+
+%green line
+
+:- discontigious station(metro, branch_av, 38.8270174,-76.9145129, 4, 1).
+:- discontigious station(metro, suitland,  38.8442565,-76.9345815, 4, 2).
+:- discontigious station(metro, naylor_rd, 38.8511242,-76.9587927, 4, 3).
+:- discontigious station(metro, southern_av, 38.8406376,-76.9777956, 4, 4).
+:- discontigious station(metro, congress_heights, 38.8450043,-76.9899239, 4, 5).
+:- discontigious station(metro, anacostia, 38.862222,-76.9973768, 4, 6).
+:- discontigious station(metro, navy_yard, 38.8743058,-77.0057141, 4, 7).
+:- discontigious station(metro, waterfront_seu, 38.8769788,-77.0197581, 4, 8).
+:- discontigious station(metro, lenfant_plaza, 38.886256,-77.0237502, 4, 9).%cruza con todas
+:- discontigious station(metro,archives, 38.893904,-77.0244128,4, 10). %cruza con amarillo
+:- discontigious station(metro, gallery_chinatown, 38.8981511,-77.0258766,4,11).%cruza con amarillo
+:- discontigious station(metro, mt_vernon_sq, 38.8992832,-77.0325695,4,12).%cruza con amarillo
+:- discontigious station(metro, shaw_howard, 38.8992832,-77.0325695,4,13).%cruza con amarillo
+:- discontigious station(metro, u_street, 38.9168309,-77.0312974, 4, 14).%cruza con amarillo
+:- discontigious station(metro, columbia_street, 38.9289062,-77.0346292,4 , 15).%cruza con amarillo
+:- discontigious station(metro, georgia_petworth, 38.9369458,-77.0263745,4 , 16).%cruza con amarillo
+:- discontigious station(metro, fort_totten, 38.9522993,-77.003775,4,17).%cruza con amarillo y rojo
+:- discontigious station(metro, west_hyatsville, 38.9555426,-76.9714943, 4, 18).
+:- discontigious station(metro, prince_george, 38.9652934,-76.9582361, 4, 19).
+:- discontigious station(metro, college_park, 38.9780383,-76.9309577, 4, 20).
+:- discontigious station(metro, belt, 39.0084165,-76.915373, 4, 21).
+
+%red line:
+:- discontigious station(metro,shady_grove,39.12,77.164722,5,1).
+:- discontigious station(metro,rockville,39.084167,77.146111,5,2).
+:- discontigious station(metro,twinbrook,39.0625,77.120833,5,3).
+:- discontigious station(metro,white_flint,39.047778,77.111667,5,4).
+:- discontigious station(metro,grosvenor_strathmore,39.029167,77.103889,5,5).
+:- discontigious station(metro,medical_center,38.998333,77.097222,5,6).
+:- discontigious station(metro,bethesda,38.984722,77.094722,5r7).
+:- discontigious station(metro,friendship_heights,38.960833,77.086111,5r8).
+:- discontigious station(metro,tenleytown_au,38.948056,77.079444,5r9).
+:- discontigious station(metro,van_ness_udc,38.944444,77.063611,5,10).
+:- discontigious station(metro,cleveland_park,38.935833,77.058611,5,11).
+:- discontigious station(metro,woodley_park,38.924444,77.0525,5,12).
+:- discontigious station(metro,dupont_circle,38.909444,77.043056,5,13).
+:- discontigious station(metro,farragut_north,38.903333,77.039722,5,14).
+:- discontigious station(metro,metro_center,38.898333,77.028056,5r15).
+:- discontigious station(metro,gallery_place,38.898889,77.021667,5,16).
+:- discontigious station(metro,judiciary_square,38.896111,77.016667,5,17).
+:- discontigious station(metro,union_:- discontigious station,38.897778,77.006667,5,18).
+:- discontigious station(metro,noma_gallaudet_u,38.906667,77.003333,5,19).
+:- discontigious station(metro,rhode_island_avenue_brentwood,38.920833,76.996111,5,20).
+:- discontigious station(metro,brookland_cua,38.933333,76.994444,5,21).
+:- discontigious station(metro,fort_totten,38.951667,77.002222,5,22).
+:- discontigious station(metro,takoma,38.975556,77.017778,5,23).
+:- discontigious station(metro,silver_spring,38.993889,77.031389,5,24).
+:- discontigious station(metro,forest_glen,39.015278,77.042778,5.25).
+:- discontigious station(metro,wheaton,39.038611,77.051111,5,26).
+:- discontigious station(metro,glenmont,39.062222,77.053333,5,27).
+
+
